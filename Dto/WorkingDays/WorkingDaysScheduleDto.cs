2e1adff19323c5f8b2bd2660958b0eb3763d787c
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:30:38 +0500

    Add: WorkingDaysScheduleDto.cs - Working days schedule data transfer object

---
+++ Dto/WorkingDays/WorkingDaysScheduleDto.cs
@@ -0,0 +1,14 @@
+namespace HRMS_BACKEND.Dto.WorkingDays;
+
+public class WorkingDaysScheduleDto
+{
+    public DateOnly EffectiveFromDate { get; set; }
+    public DateOnly? EffectiveToDate { get; set; }
+    public bool Monday { get; set; }
+    public bool Tuesday { get; set; }
+    public bool Wednesday { get; set; }
+    public bool Thursday { get; set; }
+    public bool Friday { get; set; }
+    public bool Saturday { get; set; }
+    public bool Sunday { get; set; }
+}
