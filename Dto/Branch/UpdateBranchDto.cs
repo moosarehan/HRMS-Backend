8c2ae6927cf60c9fb4fef001c258f4cc9c0c6b1e
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:29:00 +0500

    Add: UpdateBranchDto.cs - Update branch request data transfer object

---
+++ Dto/Branch/UpdateBranchDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HRMS_BACKEND.Dto.Branch;
+
+public class UpdateBranchDto
+{
+    [Required, MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(100)]
+    public string? City { get; set; }
+
+    [MaxLength(20)]
+    public string? Phone { get; set; }
+
+    [MaxLength(200)]
+    public string? Address { get; set; }
+}
