f5cc222592c95c01313f1c05f441259815aab4fc
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:42:27 +0500

    Add: 20260802173141_AddLateThresholdSnapshotToAttendance.cs - Add threshold snapshot to attendance

---
+++ Migrations/20260802173141_AddLateThresholdSnapshotToAttendance.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRMS_BACKEND.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLateThresholdSnapshotToAttendance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LateThresholdMinutesSnapshot",
+                table: "EmployeeAttendances",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LateThresholdMinutesSnapshot",
+                table: "EmployeeAttendances");
+        }
+    }
+}
