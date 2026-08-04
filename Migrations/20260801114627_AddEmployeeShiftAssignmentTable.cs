b3fa6d99abcfd0e738a7f1b611fd493a25974040
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:42:04 +0500

    Add: 20260801114627_AddEmployeeShiftAssignmentTable.cs - Add EmployeeShiftAssignment table

---
+++ Migrations/20260801114627_AddEmployeeShiftAssignmentTable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRMS_BACKEND.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmployeeShiftAssignmentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeShiftAssignments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    ShiftId = table.Column<int>(type: "int", nullable: false),
+                    EffectiveFrom = table.Column<DateOnly>(type: "date", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeShiftAssignments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeeShiftAssignments_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeShiftAssignments_Shifts_ShiftId",
+                        column: x => x.ShiftId,
+                        principalTable: "Shifts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeShiftAssignments_Employee_Date",
+                table: "EmployeeShiftAssignments",
+                columns: new[] { "EmployeeId", "EffectiveFrom" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeShiftAssignments_ShiftId",
+                table: "EmployeeShiftAssignments",
+                column: "ShiftId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeShiftAssignments");
+        }
+    }
+}
