d5a67aad3b4aceab6981872d19658ab32d25af66
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:29:55 +0500

    Add: LeaveTypeDto.cs - Leave type data transfer object

---
+++ Dto/Leave/LeaveTypeDto.cs
@@ -0,0 +1,8 @@
+namespace HRMS_BACKEND.Dto.Leave;
+
+public class LeaveTypeDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
