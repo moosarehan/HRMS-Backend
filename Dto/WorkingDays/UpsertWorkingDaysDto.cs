e40e028b6548f5bc2bb85f21f6cd02f0cbe6a9da
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:30:37 +0500

    Add: UpsertWorkingDaysDto.cs - Upsert working days data transfer object

---
+++ Dto/WorkingDays/UpsertWorkingDaysDto.cs
@@ -0,0 +1,12 @@
+namespace HRMS_BACKEND.Dto.WorkingDays;
+
+public class UpsertWorkingDaysDto
+{
+    public bool Monday { get; set; }
+    public bool Tuesday { get; set; }
+    public bool Wednesday { get; set; }
+    public bool Thursday { get; set; }
+    public bool Friday { get; set; }
+    public bool Saturday { get; set; }
+    public bool Sunday { get; set; }
+}
