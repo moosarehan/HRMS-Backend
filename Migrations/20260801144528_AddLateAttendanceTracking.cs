58061a1a100c31cebd1adc017ca860bd2b085137
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:42:06 +0500

    Add: 20260801144528_AddLateAttendanceTracking.cs - Add late attendance tracking fields

---
+++ Migrations/20260801144528_AddLateAttendanceTracking.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRMS_BACKEND.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLateAttendanceTracking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LateThresholdMinutes",
+                table: "Shifts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLate",
+                table: "EmployeeAttendances",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Shifts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "LateThresholdMinutes",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Shifts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "LateThresholdMinutes",
+                value: 15);
+
+            migrationBuilder.UpdateData(
+                table: "Shifts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "LateThresholdMinutes",
+                value: 15);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LateThresholdMinutes",
+                table: "Shifts");
+
+            migrationBuilder.DropColumn(
+                name: "IsLate",
+                table: "EmployeeAttendances");
+        }
+    }
+}
