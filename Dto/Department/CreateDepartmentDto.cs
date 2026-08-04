c491a8b5b77184cff51c713dcf08e346a2835a26
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:29:06 +0500

    Add: CreateDepartmentDto.cs - Create department request data transfer object

---
+++ Dto/Department/CreateDepartmentDto.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HRMS_BACKEND.Dto.Department;
+
+public class CreateDepartmentDto
+{
+    [Required, MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [MaxLength(500)]
+    public string? Description { get; set; }
+
+    [Required, MaxLength(100)]
+    public string BranchName { get; set; } = string.Empty;
+}
