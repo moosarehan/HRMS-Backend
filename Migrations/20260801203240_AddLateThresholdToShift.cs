a41e952b5e4728071fb02bf79e21eff39d8abb8e
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:42:25 +0500

    Add: 20260801203240_AddLateThresholdToShift.cs - Add late threshold configuration to Shift

---
+++ Migrations/20260801203240_AddLateThresholdToShift.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRMS_BACKEND.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLateThresholdToShift : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
