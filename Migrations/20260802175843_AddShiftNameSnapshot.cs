c5ce512f503236e8f99af9574aa3494d3943edf6
moosarehan <moosarehan4@gmail.com>
2026-08-04 14:42:28 +0500

    Add: 20260802175843_AddShiftNameSnapshot.cs - Add shift name snapshot to records

---
+++ Migrations/20260802175843_AddShiftNameSnapshot.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRMS_BACKEND.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShiftNameSnapshot : Migration
+        {
+            /// <inheritdoc />
+            protected override void Up(MigrationBuilder migrationBuilder)
+            {
+                migrationBuilder.AddColumn<string>(
+                    name: "ShiftNameSnapshot",
+                    table: "EmployeeAttendances",
+                    type: "nvarchar(max)",
+                    nullable: true);
+            }
+
+            /// <inheritdoc />
+            protected override void Down(MigrationBuilder migrationBuilder)
+            {
+                migrationBuilder.DropColumn(
+                    name: "ShiftNameSnapshot",
+                    table: "EmployeeAttendances");
+            }
+        }
+}
